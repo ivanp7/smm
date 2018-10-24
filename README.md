da2c2e952c3acb1b1cceb755fe5bc075759f3bd0
ivanp7 <ivanp7.git@gmail.com>
2018-10-24 21:11:27 +0300

    Implement "help" and "version" actions

--- README.md
+++ README.md
@@ -1,38 +1,4 @@
-Semantic machine memory
-=======================
+Semantic machine memory tool
+============================
 
-Usage:
-------
-
-**smm** *mode* *command* *[arguments]*
-
-where:
-*mode* is either "root", "body"
-
-Supported operations:
----------------------
-
-- smm init
-
-- smm path \<path\>
-- smm ls \<path\>
-- smm mkdir \<path\>
-- smm rm \<path\[/name\]\>
-- smm cp \<src\[/name\]\> \<dest\[/name\]\>
-- smm mv \<src\[/name\]\> \<dest\[/name\]\>
-- smm new \<path/name\>
-
-- smm params-of \<path/name\> path
-- smm data-of \<path/name\> path
-
-- smm body-of \<path/name\> path \<path\>
-- smm body-of \<path/name\> ls \<path\>
-- smm body-of \<path/name\> mkdir \<path\>
-- smm body-of \<path/name\> rm \<path\[/name\]\>
-- smm body-of \<path/name\> cp \<src\[/name\]\> \<dest\[/name\]\>
-- smm body-of \<path/name\> mv \<src\[/name\]\> \<dest\[/name\]\>
-- smm body-of \<path/name\> new \<path/name\> of \<path/name\>
-
-- smm body-of \<path/name\> appl-of \<path/name\> prototype path
-- smm body-of \<path/name\> appl-of \<path/name\> args path
 
--- src/smmt.c
+++ src/smmt.c
@@ -1,15 +1,62 @@
 #include <locale.h>
+#include <wchar.h>
 
 #include "smmt.h"
-#include "smmt_args.ggo.h"
+#include "smmt_cmdline.ggo.h"
+
+void print_parsing_error(const char *reason)
+{
+    if(!strcmp(reason, "unknown"))
+    {
+        printf("Error: unknown action supplied.\n");
+    }
+    else if(!strcmp(reason, "too_many"))
+    {
+        printf("Error: too many arguments supplied.\n");
+    }
+}
 
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "");
     
-    struct gengetopt_args_info ai;
-    if (cmdline_parser(argc, argv, &ai) != 0) 
+    if (argc == 1)
     {
+        cmdline_parser_print_help();
+        return 0;
+    }
+
+    struct gengetopt_args_info ai;
+    if (cmdline_parser(argc-1, argv+1, &ai) != 0) 
+    {
+        return 1;
+    }
+
+    char *action = argv[1];
+
+    if (!strcmp(action, "help"))
+    {
+        if (argc > 2)
+        {
+            print_parsing_error("too_many");
+            return 1;
+        }
+
+        cmdline_parser_print_help();
+    }
+    else if (!strcmp(action, "version"))
+    {
+        if (argc > 2)
+        {
+            print_parsing_error("too_many");
+            return 1;
+        }
+
+        cmdline_parser_print_version();
+    }
+    else
+    {
+        print_parsing_error("unknown");
         return 1;
     }
 
